0d5e8f75e62f3fe406f4301e08efbb9937c28b19
Justin Harrison <justin@matthin.com>
2015-05-17 16:59:23 -0400

    Forgot to remove generated shipment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
-  get 'shipment/create'
-  end
-
-  namespace :api do
-  get 'shipment/index'
-  end
-
-  namespace :api do
-  get 'shipment/show'
-  end
-
   namespace(:api, defaults: {format: "json"},
             constraints: {subdomain: "api"}, path: "/") do
     resources(:parcel)

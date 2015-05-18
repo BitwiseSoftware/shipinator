544c17d67f92ceb5dc1fe20b35a1cfcd4d0376c8
Justin Harrison <justin@matthin.com>
2015-05-18 13:22:52 -0400

    Add return_address to shipment

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,14 @@
 class Shipment < ActiveRecord::Base
   validates(:origin_id, :destination_id, presence: true)
+  before_save(:validate_return_address)
 
   belongs_to(:address, class_name: "origin")
   belongs_to(:address, class_name: "destination")
+  belongs_to(:address, class_name: "return_address")
+
+private
+  def validate_return_address
+    self.return_address_id ||= origin_id
+  end
 end
 
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -14,5 +14,19 @@ RSpec.describe(Shipment, type: :model) do
       expect(FactoryGirl.build(:shipment, destination_id: nil)).to_not(be_valid)
     end
   end
+
+  context("when return_address") do
+    it("is nil, sets it to origin") do
+      shipment = FactoryGirl.create(:shipment, return_address_id: nil)
+      expect(shipment.return_address_id).to(eq(shipment.origin_id))
+    end
+
+    it("exists, does nothing") do
+      shipment = FactoryGirl.build(:shipment)
+      return_address_id = shipment.return_address_id
+      shipment.save!
+      expect(shipment.return_address_id).to(eq(return_address_id))
+    end
+  end
 end
 
